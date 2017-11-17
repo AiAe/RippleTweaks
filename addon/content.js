b7936dc53590b9f2e325a99d749ec1b3a38d2087
Daniel Dimitrov <bgdppeu@gmail.com>
2017-11-17 18:44:52 +0200

    Added sending theme to server

--- addon/content.js
+++ addon/content.js
@@ -2,7 +2,13 @@ $(document).ready(function () {
 	var url = window.location.href;
 	var user_id = url.split('/')[4].split('?')[0];	
 	var mode = url.split('=')[1];
-	
+
+	var dark = false;
+    var c = document.cookie.split(" ");
+    for (var i = 0; i < c.length; i++)
+        if (c[i].startsWith("cflags"))
+            dark = c[i].split("=")[1].startsWith("1")
+
 	var std = 'hidden'; 
 	var taiko = 'hidden';
 	var ctb = 'hidden';
@@ -18,11 +24,11 @@ $(document).ready(function () {
 		mania = '';
 	}
 	
-	$("<div data-mode='0' " + std + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/0/'></iframe></div>").insertAfter( "#mode-menu" );
+	$("<div class='ui raised segment twemoji' id='userpage-content' data-mode='0' " + std + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/0/" + dark + "/'></iframe></div>").insertAfter( "#mode-menu" );
 	
-	$("<div data-mode='1' " + taiko + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/1/'></iframe></div>").insertAfter( "#mode-menu" );
+	$("<div class='ui raised segment twemoji' id='userpage-content' data-mode='1' " + taiko + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/1/" + dark + "/'></iframe></div>").insertAfter( "#mode-menu" );
 	
-	$("<div data-mode='2' " + ctb + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/2/'></iframe></div>").insertAfter( "#mode-menu" );
+	$("<div class='ui raised segment twemoji' id='userpage-content' data-mode='2' " + ctb + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/2/" + dark + "/'></iframe></div>").insertAfter( "#mode-menu" );
 	
-	$("<div data-mode='3' " + mania + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/3/'></iframe></div>").insertAfter( "#mode-menu" );
+	$("<div class='ui raised segment twemoji' id='userpage-content' data-mode='3' " + mania + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/3/" + dark + "/'></iframe></div>").insertAfter( "#mode-menu" );
 });
