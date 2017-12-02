62ad7321bd6adbf00b8fbb27dafe75e63b5a5780
Daniel Dimitrov <bgdppeu@gmail.com>
2017-12-02 02:47:44 +0200

    Thanks [Hidekazu] :eggplant:

--- addon/content.js
+++ addon/content.js
@@ -1,34 +1,21 @@
 $(document).ready(function () {
-	var url = window.location.href;
-	var user_id = url.split('/')[4].split('?')[0];	
-	var mode = url.split('=')[1];
+        var url = window.location.href;
+        var user_id = url.split('/')[4].split('?')[0];
+        var mode = url.split('=')[1];
 
-	var dark = false;
-    var c = document.cookie.split(" ");
-    for (var i = 0; i < c.length; i++)
-        if (c[i].startsWith("cflags"))
-            dark = c[i].split("=")[1].startsWith("1")
+        var dark = false;
+        var c = document.cookie.split(" ");
+        for (var i = 0; i < c.length; i++)
+            if (c[i].startsWith("cflags"))
+                dark = c[i].split("=")[1].startsWith("1");
 
-	var std = 'hidden'; 
-	var taiko = 'hidden';
-	var ctb = 'hidden';
-	var mania = 'hidden';
-	
-	if (mode === "0"){
-		std = '';
-	}else if(mode === "1"){
-		taiko = '';
-	}else if(mode === "2"){
-		ctb = '';
-	} else if(mode === "3"){
-		mania = '';
-	}
-	
-	$("<div class='ui raised segment twemoji' id='userpage-content' data-mode='0' " + std + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/0/" + dark + "/'></iframe></div>").insertAfter( "#mode-menu" );
-	
-	$("<div class='ui raised segment twemoji' id='userpage-content' data-mode='1' " + taiko + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/1/" + dark + "/'></iframe></div>").insertAfter( "#mode-menu" );
-	
-	$("<div class='ui raised segment twemoji' id='userpage-content' data-mode='2' " + ctb + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/2/" + dark + "/'></iframe></div>").insertAfter( "#mode-menu" );
-	
-	$("<div class='ui raised segment twemoji' id='userpage-content' data-mode='3' " + mania + "><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='http://127.0.0.1:7001/" + user_id + "/3/" + dark + "/'></iframe></div>").insertAfter( "#mode-menu" );
-});
+        var modes = [
+            {mode: 0, visible: mode === '0'},
+            {mode: 1, visible: mode === '1'},
+            {mode: 2, visible: mode === '2'},
+            {mode: 3, visible: mode === '3'}
+        ];
+
+        for(var m of modes)
+            $(`<div class='ui raised segment twemoji' id='userpage-content' data-mode='${m.mode}' ${!m.visible ? 'hidden':'visible'}><iframe frameBorder='0' scrolling='no' width='100%' height='400px' src='https://rippletweaks.aiae.ovh/${user_id}/${m.mode}/${dark}/'></iframe></div>`).insertAfter( "#mode-menu" );
+    });
